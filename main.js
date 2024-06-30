43b254a590f51e2c1c4653cca778d7382cb99a1f
Ball-Thitiwut <Thitiwut_Thitnuea@cmu.ac.th>
2024-06-30 17:35:11 +0700

    Q4.9

--- main.js
+++ main.js
@@ -8,13 +8,15 @@ createApp({
         const productLink = ref('https://www.camt.cmu.ac.th') 
         const inStock = ref(true)
         const inventory = ref(100)
+        const onSale = ref(true)
         return {
             product,
             description,
             image,
             productLink,
             inStock,
-            inventory
+            inventory,
+            onSale
         }
     }
 
