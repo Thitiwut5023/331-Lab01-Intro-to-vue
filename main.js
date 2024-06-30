e5e78637aa09b43d06d12fee72e4fda0127c24ed
Ball-Thitiwut <Thitiwut_Thitnuea@cmu.ac.th>
2024-06-30 17:23:24 +0700

    Q4.3

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ createApp({
         const description = ref('These are sturdy and comfortable boots suitable for all terrains.')
         const image = ref('./assets/images/socks_green.jpg')
         const productLink = ref('https://www.camt.cmu.ac.th') 
+        const inStock = ref(true)
         return {
             product,
             description,
             image,
-            productLink
+            productLink,
+            inStock
         }
     }
 
