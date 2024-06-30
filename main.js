7e716c7f008f55cd59bc0703477b58e41eb2ceb7
Ball-Thitiwut <Thitiwut_Thitnuea@cmu.ac.th>
2024-06-30 17:18:20 +0700

    Q3.6

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ createApp({
     setup(){
         const product = ref('Socks')
         const description = ref('These are sturdy and comfortable boots suitable for all terrains.')
-        const image = ref('./assets/images/socks_green.jpg') 
+        const image = ref('./assets/images/socks_green.jpg')
+        const productLink = ref('https://www.camt.cmu.ac.th') 
         return {
             product,
             description,
-            image
+            image,
+            productLink
         }
     }
 
