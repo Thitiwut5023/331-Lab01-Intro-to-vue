4ea613a130b42a31da99d504257e2eff222cf725
Ball-Thitiwut <Thitiwut_Thitnuea@cmu.ac.th>
2024-06-30 16:53:36 +0700

    Q2.5

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const { createApp, ref } = Vue
 createApp({
     setup(){
         const product = ref('Boots')
+        const description = ref('These are sturdy and comfortable boots suitable for all terrains.') 
         return {
-            product
+            product,
+            description
         }
     }
 
