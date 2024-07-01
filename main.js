60e995ae710820bbce854ddeaab10c10bc45c743
Ball-Thitiwut <Thitiwut_Thitnuea@cmu.ac.th>
2024-07-01 23:40:31 +0700

    Q10.5

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ const app = createApp({
     setup(){
         const cart = ref([])
         const premium = ref(true)
-        function updateCart(id) {
-            cart.value.push(id)
+        function updateCart(_id) {
+            for (var i = 0; i < cart.value.length; i++) {
+                if(cart.value[i].id === _id){
+                    cart.value[i].amount++;
+                    return;
+                }
+            }
+            cart.value.push({id:_id, amount: 1})
+            return;
         }
         const details = ref(["50% cotton", "30% wool", "20% polyester"])
         return {
